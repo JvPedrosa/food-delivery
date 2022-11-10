0bea095965fb9cdd282009b9da11e625a881b04a
Joao Victor <joao.victor@lieno.com.br>
2022-11-10 10:21:47 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { AccountBalanceWalletRounded, Chat, Favorite, HomeRounded, Settings, SummarizeRounded } from '@mui/icons-material';
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
+import MenuContainer from './components/MenuContainer';
 
 function App() {
+  useEffect(() => {
+    const menuLi = document.querySelectorAll('#menu li');
+
+    function setMenuActive() {
+      menuLi.forEach(n => n.classList.remove('active'));
+      this.classList.add('active')
+    }
+
+    menuLi.forEach(n => n.addEventListener('click', setMenuActive))
+  }, [])
+
   return (
     <div className="App">
       <Header />
+
+      <main>
+        <div className="mainContainer"></div>
+        <div className="rightMenu"></div>
+      </main>
+
+      <div className="bottomMenu">
+        <ul id="menu">
+          <MenuContainer link={'#'} icon={<HomeRounded />} />
+          <MenuContainer link={'#'} icon={<Chat />} />
+          <MenuContainer link={'#'} icon={<AccountBalanceWalletRounded />} />
+          <MenuContainer link={'#'} icon={<Favorite />} />
+          <MenuContainer link={'#'} icon={<SummarizeRounded />} />
+          <MenuContainer link={'#'} icon={<Settings />} />
+          <div className="indicator"></div>
+        </ul>
+      </div>
     </div>
   );
 }
